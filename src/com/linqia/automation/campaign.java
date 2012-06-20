b3a9c4e673eac2e9f300eed2b58c3bbc871b173f
shikha882 <shikha882@gmail.com>
2012-06-20 08:51:28 -0700

    Updated files after demo completion.

--- src/com/linqia/automation/campaign.java
+++ src/com/linqia/automation/campaign.java
@@ -22,8 +22,12 @@ public class campaign {
 	private String baseUrl;
 	private FirefoxDriver driver;
 	private StringBuffer verificationErrors = new StringBuffer();
+	String user;
+	String pwd;
+	String campaignName;
+	String keyword;
 
-	@Parameters({ "browser", "Url", "firefoxProfile"})
+	@Parameters({ "browser", "Url", "firefoxProfile" })
 	@BeforeTest
 	public void setUp(String browser, String Url, String firefoxProfile) throws Exception {
 		System.out.println("browser is " + browser);
@@ -37,7 +41,7 @@ public class campaign {
 	 }
 	
 	@Parameters({ "user", "pwd", "campaignName", "keyword" })
-	@Test
+	@Test( )
 	public void testDraftCampaign(String user, String pwd, String campaignName, String keyword) throws Exception {
 		driver.get(baseUrl + "/login.php");
 		driver.findElement(By.name("username")).clear();
@@ -56,8 +60,8 @@ public class campaign {
 		driver.findElement(By.id("brand")).sendKeys("Social Mom");
 		driver.findElement(By.id("name")).clear();
 		driver.findElement(By.id("name")).sendKeys(campaignName);
-		driver.findElement(By.id("start_alt")).click();
-		driver.findElement(By.linkText("19")).click();
+		driver.findElement(By.xpath(".//*[@id='start_alt']")).click();
+		driver.findElement(By.linkText("20")).click();
 		driver.findElement(By.id("duration-14")).click();
 		driver.findElement(By.id("purpose-1")).click();
 		driver.findElement(By.id("purpose-3")).click();
@@ -80,11 +84,11 @@ public class campaign {
 		driver.findElement(By.id("content-ins")).sendKeys("Website for downloading coupons.");
 		driver.findElement(By.linkText("Supporting text")).click();
 		driver.findElement(By.id("name")).clear();
-		driver.findElement(By.id("name")).sendKeys("Social mom and shopping");
+		driver.findElement(By.xpath("(.//*[@id='name'])[1]")).sendKeys("Social mom and shopping");
 		driver.findElement(By.id("text")).clear();
 		driver.findElement(By.id("text")).sendKeys("Social moms shop online for cosmetics, hair accessories, nail paints and other home stuff.");
 		driver.findElement(By.id("submit")).click();
-		driver.findElement(By.linkText("Links")).click();
+		driver.findElement(By.xpath(".//*[@id='content-list']/div[1]/div[2]/a")).click();
 		driver.findElement(By.xpath("(//input[@id='name'])[2]")).clear();
 		driver.findElement(By.xpath("(//input[@id='name'])[2]")).sendKeys("Social mom");
 		driver.findElement(By.cssSelector("#link-element > div.placeholder")).click();
@@ -94,29 +98,56 @@ public class campaign {
 		driver.findElement(By.id("link")).clear();
 		driver.findElement(By.id("link")).sendKeys("socialmom.com");
 		driver.findElement(By.xpath("(//input[@id='submit'])[2]")).click();
-		driver.findElement(By.linkText("Videos")).click();
+		driver.findElement(By.xpath(".//*[@id='content-list']/div[1]/div[4]/a")).click();
 		driver.findElement(By.xpath("(//input[@id='name'])[4]")).clear();
-		driver.findElement(By.xpath("(//input[@id='name'])[4]")).sendKeys("Social mom and");
+		driver.findElement(By.xpath("(//input[@id='name'])[4]")).sendKeys("Social mom and Shopping");
 		driver.findElement(By.xpath("(//input[@id='link_name'])[2]")).clear();
 		driver.findElement(By.xpath("(//input[@id='link_name'])[2]")).sendKeys("How social mom shop");
 		driver.findElement(By.cssSelector("#content-video-form > dl.zend_form > #link-element > div.placeholder")).click();
 		driver.findElement(By.xpath("(//input[@id='link'])[3]")).clear();
 		driver.findElement(By.xpath("(//input[@id='link'])[3]")).sendKeys("http://youtu.be/sNqaXt1F0RA");
 		driver.findElement(By.xpath("(//input[@id='submit'])[3]")).click();
-		//driver.findElement(By.linkText("NEXT STEP")).click();
 		driver.findElement(By.xpath("//input[@class='next-step']")).click();
-		driver.findElement(By.xpath("//input[@class='save-draft resetStyle']")).click();
-		//driver.findElement(By.linkText("Save as draft")).click();
+		driver.findElement(By.xpath(".//*[@id='campaigns-engagement-summary']/div[3]/div/div[2]/div/div[1]/div[4]/form/input")).click();
 		driver.findElement(By.xpath("//*[@id='campaigns']/aside/nav/a[2]")).click();
 		driver.findElement(By.linkText("Delete")).click();
 		Alert alert = driver.switchTo().alert();
 		String alertname = alert.getText();
 		System.out.println("Alert name is " + alertname);
 		alert.accept();
-		// ERROR: Caught exception [ERROR: Unsupported command [getConfirmation]]
 		driver.findElement(By.linkText("Logout")).click();
 	}
 	
+	@Parameters({ "user", "pwd", "campaignName", "keyword" })
+	@Test( )
+	public void testOverlayDeletion(String user, String pwd, String campaignName, String keyword) throws Exception {
+		driver.get(baseUrl + "/login.php");
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("username")).sendKeys(user);
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys(pwd);
+		driver.findElement(By.cssSelector("button.actionButton.fleft")).click();
+		driver.findElement(By.linkText("Create campaign")).click();
+		driver.findElement(By.name("search")).clear();
+		driver.findElement(By.name("search")).sendKeys(keyword);
+		driver.findElement(By.name("do-search")).click();
+		Thread.currentThread();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(".//*[@id='groups']/table/thead/tr/th[3]")).isDisplayed();
+		driver.findElement(By.xpath(".//*[@id='groups']/table/tbody/tr[last()-1]/td[6]/div/a[1]")).click();
+		driver.findElement(By.xpath(".//*[@id='groups']/table/thead/tr/th[3]")).isDisplayed();
+		driver.findElement(By.xpath(".//*[@id='groups']/table/tbody/tr[last()-2]/td[6]/div/a[1]")).click();
+		driver.findElement(By.xpath(".//*[@id='groups']/table/thead/tr/th[3]")).isDisplayed();
+		//driver.findElement(By.xpath(".//*[@id='groups']/table/tbody/tr[last()-4]/td[6]/div/a[1]")).click();
+		//driver.findElement(By.xpath(".//*[@id='groups']/table/thead/tr/th[3]")).isDisplayed();
+		driver.findElement(By.xpath(".//*[@id='page-form']/div[1]/div[2]/table/tbody/tr[5]/td/a")).click();
+		driver.findElement(By.xpath(".//*[@id='recap-detail-wrapper']/h2")).isDisplayed();
+		driver.findElement(By.xpath(".//*[@id='recap-detail']/tbody/tr[last()]/td[6]/a/img")).click();
+		driver.findElement(By.xpath(".//*[@id='recap-detail']/tbody/tr[last()]/td[6]/a/img")).click();
+		driver.findElement(By.xpath(".//*[@id='cboxClose']")).click();
+		driver.findElement(By.linkText("Logout")).click();
+	}
+
 	@AfterTest
 	public void tearDown() throws Exception {
 		driver.quit();
